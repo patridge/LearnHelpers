cd019d2e00e8cab1c806a92d5aaae43bc90a4508
Adam Patridge <patridge@gmail.com>
2021-04-23 08:39:47 -0600

    Make content query generic for module or path

--- js/site.js
+++ js/site.js
@@ -9,21 +9,21 @@
         const query = findTechnicalReviewByUidQuery.replace("{{uid}}", uid);
         return createAdHocQueryUrl(query);
     };
-    const findModuleByUidQuery = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State] FROM workitems WHERE [System.TeamProject] = 'Microsoft Learn' AND [System.WorkItemType] = 'Module' AND [Custom.UID] Contains '{{uid}}'";
-    const createUidModuleQuery = (uid) => {
-        const query = findModuleByUidQuery.replace("{{uid}}", uid);
+    const findModuleOrPathByUidQuery = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State] FROM workitems WHERE [System.TeamProject] = 'Microsoft Learn' AND [System.WorkItemType] In ('Module','Learning Path') AND [Custom.UID] Contains '{{uid}}'";
+    const createUidModuleOrPathQuery = (uid) => {
+        const query = findModuleOrPathByUidQuery.replace("{{uid}}", uid);
         return createAdHocQueryUrl(query);
     };
     // NOTE: Won't find issues when the module UID isn't present in the unit UIDs (e.g., differ in prefix).
-    const findModuleIssuesByUidQuery = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State] FROM workitems WHERE [System.TeamProject] = 'Microsoft Learn' AND [System.WorkItemType] = 'Customer Feedback' AND [Feedback Source]='Report an issue' AND [Custom.UID] Contains '{{uid}}' AND [State] NOT IN ('Closed', 'Rejected', 'Declined')";
+    const findCustomerIssuesByUidQuery = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State] FROM workitems WHERE [System.TeamProject] = 'Microsoft Learn' AND [System.WorkItemType] = 'Customer Feedback' AND [Feedback Source]='Report an issue' AND [Custom.UID] Contains '{{uid}}' AND [State] NOT IN ('Closed', 'Rejected', 'Declined')";
     const createUidIssuesQuery = (uid) => {
-        const query = findModuleIssuesByUidQuery.replace("{{uid}}", uid);
+        const query = findCustomerIssuesByUidQuery.replace("{{uid}}", uid);
         return createAdHocQueryUrl(query);
     };
     // NOTE: Won't find issues when the module UID isn't present in the unit UIDs (e.g., differ in prefix).
-    const findModuleVerbatimsByUidQuery = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State] FROM workitems WHERE [System.TeamProject] = 'Microsoft Learn' AND [System.WorkItemType] = 'Customer Feedback' AND [Feedback Source]='Star rating verbatim' AND [Custom.UID] Contains '{{uid}}' AND [State] NOT IN ('Closed', 'Rejected', 'Declined')";
+    const findVerbatimsByUidQuery = "SELECT [System.Id],[System.WorkItemType],[System.Title],[System.AssignedTo],[System.State] FROM workitems WHERE [System.TeamProject] = 'Microsoft Learn' AND [System.WorkItemType] = 'Customer Feedback' AND [Feedback Source]='Star rating verbatim' AND [Custom.UID] Contains '{{uid}}' AND [State] NOT IN ('Closed', 'Rejected', 'Declined')";
     const createUidVerbatimsQuery = (uid) => {
-        const query = findModuleVerbatimsByUidQuery.replace("{{uid}}", uid);
+        const query = findVerbatimsByUidQuery.replace("{{uid}}", uid);
         return createAdHocQueryUrl(query);
     };
 
@@ -36,14 +36,14 @@
     const addUidResult = function (uid) {
         const uidResult = uidResultTemplate.content.cloneNode(true);
         const uidSpan = uidResult.querySelector(".uid");
-        const uidModuleQueryUrlAnchor = uidResult.querySelector(".uid-query-module-url");
+        const uidModuleOrPathQueryUrlAnchor = uidResult.querySelector(".uid-query-content-work-item-url");
         const uidReviewQueryUrlAnchor = uidResult.querySelector(".uid-query-review-url");
         const uidIssuesQueryUrlAnchor = uidResult.querySelector(".uid-issues-url");
         const uidVerbatimsQueryUrlAnchor = uidResult.querySelector(".uid-verbatims-url");
 
         uidSpan.innerText = `"${uid}"`;
-        const uidQueryModulesUrl = createUidModuleQuery(uid);
-        uidModuleQueryUrlAnchor.setAttribute("href", uidQueryModulesUrl);
+        const uidQueryModuleOrPathUrl = createUidModuleOrPathQuery(uid);
+        uidModuleOrPathQueryUrlAnchor.setAttribute("href", uidQueryModuleOrPathUrl);
         const uidQueryReviewsUrl = createUidReviewQuery(uid);
         uidReviewQueryUrlAnchor.setAttribute("href", uidQueryReviewsUrl);
         const uidIssuesUrl = createUidIssuesQuery(uid);
